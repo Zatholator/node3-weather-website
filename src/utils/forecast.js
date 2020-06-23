1a39d6c56fc8b8501d30c2f673877de72dc55c0d
Kevin Davis <kdavis@tamu.edu>
2020-06-23 11:07:27 +0300

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,23 @@
+const request = require('request')
+
+const forecast = (lat, lon, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=30a01a36248313337b7d2c593465e0aa&query=' + lat + ',' + lon + '&units=f'
+
+    //request({ url: url, json: true }, (error, response) => {
+    request({ url, json: true }, (error, { body } ) => {
+        if (error) {
+            callback('Unable to connect to weather service.', undefined)
+        //} else if (response.body.error){
+        } else if (body.error){
+
+            callback('Unable to find location.', undefined)
+        } else {
+            //callback(undefined, `${response.body.current.weather_descriptions[0]}. Currently it is ${response.body.current.temperature} F and it feels like ${response.body.current.feelslike} F.`)
+            callback(undefined, `${body.current.weather_descriptions[0]}. Currently it is ${body.current.temperature} F and it feels like ${body.current.feelslike} F.`)
+        }
+    })
+}
+
+
+
+module.exports = forecast
